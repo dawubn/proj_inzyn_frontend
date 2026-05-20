705051493936165beb75a45375b7c0ed83104eeb
Wesolxxx <j.wesolekk@gmail.com>
2026-05-20 21:33:51 +0200

    feat:PP-152: implement layout and improve routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+
 import ProtectedRoute from "./components/ProtectedRoute";
+import AppLayout from "./components/layout/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* public routes */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/test" element={<h1>TEST</h1>} />
+
+        {/* protected routes with layout */}
         <Route
-          path="/dashboard"
           element={
             <ProtectedRoute>
-              <Dashboard />
+              <AppLayout />
             </ProtectedRoute>
           }
-        />
+        >
+          <Route path="/dashboard" element={<Dashboard />} />
+
+          <Route
+            path="/history"
+            element={<div>History of analysis</div>}
+          />
+
+          <Route
+            path="/document-analysis"
+            element={<div>Document Analysis</div>}
+          />
+
+          <Route
+            path="/rule-profiles"
+            element={<div>Rule profiles</div>}
+          />
+
+          <Route
+            path="/account-details"
+            element={<div>Account details</div>}
+          />
+
+          <Route
+            path="/history/analysis-details"
+            element={<div>Analysis Details - temporary for breadcrumbs testing</div>}
+          />  
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
